46588afe889720df2731d35fde57d6134a6cb6cf
Damian <damian9691@wp.pl>
2022-06-12 13:57:30 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { UnauthorizedExceptionFilter } from './filters/Unauthorized-exception.fi
 import { GlobalExceptionFilter } from './filters/Global-exception.filters';
 
 import * as cookieParser from 'cookie-parser';
-console.log(process.env.NODE_ENV);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONTEND_URL],
+    origin: 'https://fedoser-a0fe6.web.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     preflightContinue: false,
